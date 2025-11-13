c0cd586725cd41bb6ac4349d56fb3ea614db8ee1
mabelbieger <165303616+mabelbieger@users.noreply.github.com>
2025-11-13 08:41:27 -0300

    ajuste backend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 const enviarEmailToken = async (email, nome, token) => {
   try {
